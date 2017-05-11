42395a8af222bfdc094db4f8c8d8ec30c296fdd2
blueburningcoder <f.karg10@gmail.com>
2017-05-11 17:08:33 +0200

    Added some patterns, basically finished exercise

--- hwp/aufg-02/blatt-02/blatt-02.ino
+++ hwp/aufg-02/blatt-02/blatt-02.ino
@@ -1,21 +1,22 @@
 // display test: write some data to the lcd
 
 /* Connections:
- R/S : Pin 3
+ R/S : Pin 3 | 2
  R/W : not connected
- E   : Pin 4
- DB4 : Pin 6
- DB5 : Pin 7
- DB6 : Pin 8
- DB7 : Pin 9
+ E   : Pin 4 | 3
+ DB4 : Pin 6 | 4
+ DB5 : Pin 7 | 5
+ DB6 : Pin 8 | 7
+ DB7 : Pin 9 | 8
 */
 
 // include LCD functions:
-#include <LiquidCrystal.h>
+// #include <LiquidCrystal.h>
 
 // define the LCD screen
 // LiquidCrystal lcd(3, 4, 6, 7, 8, 9);
 
+#define speed spd
 
 // defining values for motor-stuff
 int motorPinA1 = 10;
@@ -24,40 +25,10 @@ int motorPinB1 = 6;
 int motorPinB2 = 9;
 
 int drivingPattern = 0;
+int spirale = 10;
 
-// define ultrasound pins
-int us1 = 13;
-
-// setting motor speed for either of A or B, to speed in direction (?forward)
-void setMotorSpeed(bool motorA, uint8_t speed = 100, bool forward = true) {
-  analogWrite(motorA ? motorPinA1 : motorPinB1, speed * forward);
-  analogWrite(motorA ? motorPinA2 : motorPinB2, speed * (!forward));
-}
-
-
-// driving forward for @time with @speed
-void driveForward(int time, int speed, bool forward = true) {
-  setMotorSpeed(true, speed, forward);
-  setMotorSpeed(false, speed, forward);
-  delay(time);
-  stopMotors();
-}
-
-// 'instantly' stopping both motors.
-void stopMotors() {
-  setMotorSpeed(true, 0);
-  setMotorSpeed(false, 0);
-}
-
-// driving the curve as specified in Ex. 4 with @degree in @forward direction
-void driveCurve(int degree, bool forward = true) {
-  if (abs(degree) > 90) {
-    return;
-  }
-  setMotorSpeed(true, degree >= 0 ? 255 : map(degree, -90, 0, -255, 255));
-  setMotorSpeed(false, degree > 0 ?       map(degree, 0, 90, 255, -255) : 255);
-
-}
+// define ultrasonic pins
+int ultrasonicPin = 13;
 
 
 
@@ -68,81 +39,138 @@ void setup()
   pinMode(motorPinA2, OUTPUT);
   pinMode(motorPinB1, OUTPUT);
   pinMode(motorPinB2, OUTPUT);
-  pinMode(us1, OUTPUT);
-  digitalWrite(us1, LOW);
-  delay(500);
 
+  // declaring pin for ultrasonic sensor
+  pinMode(ultrasonicPin, OUTPUT);
+
+  delay(500);
 }
 
 
-void loop()
-{
-/*
-  // reinitializes the display, in order to remove weird behaviour.
-  // Got even weirder. But seems to be clearer and works.
-  lcd.begin(20, 4);
-  lcd.print("Analog0: ");
-  lcd.print(analogRead(0));
 
-  // writing in the second line.
-  lcd.setCursor(0, 1);
-  lcd.print("button: ");
+void loop() {
 
-  // reading the value
+  //reading the value
   int but = analogRead(0);
 
   // selecting the correct button.
   if (but <= 5) {
-    lcd.print("1");
+    drivingPattern = 1;
   } else if (but >= 238 && but <= 250) {
-    lcd.print("2");
+    drivingPattern = 2;
   } else if (but >= 468 && but <= 480) {
-    lcd.print("3");
+    drivingPattern = 3;
   } else if (but >= 670 && but <= 683) {
-    lcd.print("4");
+    drivingPattern = 4;
   } else if (but >= 820 && but <= 840) {
-    lcd.print("5");
-  } else if (but >= 1020 && but <= 1024) {
-    lcd.print("-");
-  } else {
-    lcd.print("u! ");
-    lcd.print(but);
+    drivingPattern = 5;
   }
-*/
-  // setMotorSpeed(false);
-  // delay(500);
-  // setMotorSpeed(false, 0);
 
-  driveForward(500, 100);
-  delay(100);
-  driveForward(500, 100, false);
-  delay(100);
-  driveCurve(45);
-  delay(100);
-  stopMotors();
 
-  // wait for 100 ms (reduces display flickering)
-  delay(500);
+  if (drivingPattern == 1) {
+    driveRectangle();
+  } else if (drivingPattern == 2) {
+    driveCircle();
+  } else if (drivingPattern == 3) {
+    avoidObstacles();
+  } else if (drivingPattern == 4) {
+    driveSpirale();
+  } else if (drivingPattern == 5) {
+    driveTriangle();
+  }
 }
 
 
 
+// setting motor speed for either of A or B, to speed in direction (@forward)
+void setMotorSpeed(bool motorA, uint8_t speed = 100, bool forward = true) {
+  analogWrite(motorA ? motorPinA1 : motorPinB1, speed * forward);
+  analogWrite(motorA ? motorPinA2 : motorPinB2, speed * (!forward));
+}
 
 
-/* Usefull LCD functions:
-set the current write position of the lcd to specific line and row:
-  lcd.setCursor(row, line);
 
-write onto lcd, starting at current position:
-  lcd.print("abc");
+// driving @forward for @time with @speed
+void driveForward(int time, uint8_t speed = 255, bool forward = true) {
+  setMotorSpeed(true, speed, forward);
+  setMotorSpeed(false, speed, forward);
+  delay(time);
+  stopMotors();
+}
 
-clear the lcd (this writes " " into all positions and is therefore slow):
-If only specific areas should be cleared use a mix of setCursor and print(" ") instead
-  lcd.clear();
-*/
+
+
+// 'instantly' stopping both motors
+void stopMotors() {
+  setMotorSpeed(true, 0);
+  setMotorSpeed(false, 0);
+}
+
+
+
+// driving the curve as specified in Ex. 4 with @degree in @forward direction for @time.
+void driveCurve(int degree, int time, uint8_t speed = 255, bool forward = true) {
+  if (abs(degree) > 90) {
+    return;
+  }
+  // setting specific motor speed based on direction, speed can be negative (relative)
+  int speedA = degree >= 0 ? speed : map(degree, -90, 0, -speed,  speed);
+  int speedB = degree >  0 ?         map(degree, 0, 90,   speed, -speed) : speed;
+  setMotorSpeed(true,  speedA > 0 ? speedA : -speedA, speedA > 0 ? true : false);
+  setMotorSpeed(false, speedB > 0 ? speedB : -speedB, speedB > 0 ? true : false);
+  delay(time);
+  stopMotors();
+}
+
+
+
+// driving a Rectangle
+void driveRectangle() {
+  driveForward(300, 100);
+  driveCurve(-40, 100, 100);
+}
+
+
+
+// driving a Circle
+void driveCircle() {
+  driveCurve(90, 100);
+}
+
+
+
+// driving a spirale TODO: actually do this.
+void driveSpirale() {
+  driveCurve(90, 100);
+  driveForward(1000, 100);
+  if (spirale >= 10) {
+    driveCurve(90, 100);
+  }
+}
+
+
+
+// driving the triangle TODO rework, improve
+void driveTriangle() {
+  driveCurve(60, 200, 150);
+  driveForward(600, 150);
+}
+
+
+
+// trying to avoid obstacles closer to 25cm
+void avoidObstacles() {
+  int dist = measureDistance(ultrasonicPin);
+  if ( dist < 25 && dist >= 2) {
+    driveCurve(-80, 200, 100);
+  } else {
+    driveForward(200, 100);
+  }
+}
 
 
 int measureDistance(int pin) {
+
   /*
    *  Returns distance in cm for a given pin,
    *  connected to an ultrasound sensor.
