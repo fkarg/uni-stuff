0cd96c4de0b157fbfa6874bad8ca48b650ce3f71
blueburningcoder <f.karg10@gmail.com>
2017-05-23 10:31:57 +0200

    Genegal Update

--- infoI-II/w03/maximum-hourglass/src/maximumhourglass/Main.java
+++ infoI-II/w03/maximum-hourglass/src/maximumhourglass/Main.java
@@ -5,9 +5,9 @@ public class Main {
    public static void main(String[] arg0) {
        /*  Debug
        int max = maxHourglass(new int[][]{
-           { 0, 0, 0, 0, 0, 0 },
-           { 0, 0, 0, 0, 0, 0 },
-           { 0, 0, 0, 0, 0, 0 },
+           { 1, 1, 1, 0, 0, 0 },
+           { 0, 1, 0, 0, 0, 0 },
+           { 1, 1, 0, 0, 0, 0 },
            { 0, 0, 0, 1, 1, 1 },
            { 0, 0, 0, 0, 1, 0 },
            { 0, 0, 0, 1, 1, 1 }
@@ -17,23 +17,16 @@ public class Main {
    }
 
    public static int maxHourglass(int[][] matrix) {
-       int max = 0;
-       int sum = 0;
+       // setting initial values
+       int max = Integer.MIN_VALUE;
+       int sum = Integer.MIN_VALUE;
 
        // iterating through the matrix, looking for each sum
        for (int i = 0; i < 4; i++) {
            for (int j = 0; j < 4; j++) {
-               sum = 0;
-               /* Debug
-               System.out.print(matrix[i][j]);
-               System.out.print(matrix[i][j+1]);
-               System.out.print(matrix[i][j+2]);
-               System.out.print(matrix[i+1][j+1]);
-               System.out.print(matrix[i+2][j]);
-               System.out.print(matrix[i+2][j+1]);
-               System.out.println(matrix[i+2][j+2]);
-               */
-               sum += matrix[i][j];
+               // setting sum
+               sum = Integer.MIN_VALUE;
+               sum = matrix[i][j];
                sum += matrix[i][j+1];
                sum += matrix[i][j+2];
                sum += matrix[i+1][j+1];
@@ -41,15 +34,8 @@ public class Main {
                sum += matrix[i+2][j+1];
                sum += matrix[i+2][j+2];
                if (sum > max) {
+                   // updating max if true
                    max = sum;
-                   /* Debug
-                   System.out.print("i: ");
-                   System.out.print(i);
-                   System.out.print(", j: ");
-                   System.out.print(j);
-                   System.out.print(", max: ");
-                   System.out.println(max);
-                   */
                }
            }
        }
