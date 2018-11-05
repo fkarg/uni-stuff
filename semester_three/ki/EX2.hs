1f0fedd4479daab8029aa650d538322e3c573565
fkarg <f.karg10@gmail.com>
2018-11-05 12:17:24 +0100

    update

---
+++ semester_three/ki/EX2.hs
@@ -0,0 +1,10 @@
+
+module Algorithms where
+
+
+field = (Just <$> [2, 8, 3, 1, 6, 4, 7]) ++ [Nothing, Just 5]
+final = (Just <$> [1, 2, 3, 8]) ++ return Nothing ++ (Just <$> [4, 7, 6, 5])
+
+-- astar :: 
+
+
