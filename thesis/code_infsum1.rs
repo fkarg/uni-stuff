523c1a8701d0f6efb71535fabd95d709ee6c1bed
fkarg <f.karg10@gmail.com>
2019-07-04 19:34:19 +0200

    restructuring of chapter approach. Added readable code highlighting of Rust.

---
+++ thesis/code_infsum1.rs
@@ -0,0 +1,4 @@
+// let list1 = vec![0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0];
+// let list2 = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+println!("{}, sum: {}", list1, list1.iter().sum());
+println!("{}, sum: {}", list2, list2.iter().sum());
