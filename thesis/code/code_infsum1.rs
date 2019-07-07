1876c6300bffe3de64c62bc1cd7d6087d4ce9aca
fkarg <f.karg10@gmail.com>
2019-07-07 15:17:23 +0200

    bigger general update

--- thesis/code/code_infsum1.rs
+++ thesis/code/code_infsum1.rs
@@ -1,4 +1,4 @@
 // let list1 = vec![0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0];
 // let list2 = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
-println!("{}, sum: {}", list1, list1.iter().sum());
-println!("{}, sum: {}", list2, list2.iter().sum());
+println!("sum: {}", list1.iter().sum()); // sum: 1
+println!("sum: {}", list2.iter().sum()); // sum: 0
--- thesis/code/code_infsum2.rs
+++ thesis/code/code_infsum2.rs
@@ -1,4 +1,4 @@
-// let list1 = vec![0.0, 0.0, 0.0, 0.16000000000003, 0.0, 0.0, 0.0, 0.0];
+// let list1 = vec![0.0, 0.0, 0.0, 0.16000000000000003, 0.0, 0.0, 0.0, 0.0];
 // let list2 = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
-println!("{}, sum: {}", list1, list1.iter().sum());
-println!("{}, sum: {}", list2, list2.iter().sum());
+println!("sum: {}", list1.iter().sum()); // sum: 0.4
+println!("sum: {}", list2.iter().sum()); // sum: inf
